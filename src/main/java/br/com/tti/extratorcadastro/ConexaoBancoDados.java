d9818478aa6400966b6b181b7e5343ae4be97c25
Crhistian Noriega <crhisnoriega@gmail.com>
2015-02-13 11:28:54 +0000

    teste

---
+++ src/main/java/br/com/tti/extratorcadastro/ConexaoBancoDados.java
@@ -0,0 +1,16 @@
+package br.com.tti.extratorcadastro;
+
+public class ConexaoBancoDados {
+  
+  private String url;
+  private String driver;
+  private String usuario;
+  private String senha;
+
+  @Override
+    public boolean equals(Object arg0) {
+        // TODO Auto-generated method stub
+        return super.equals(arg0);
+    }
+
+}
