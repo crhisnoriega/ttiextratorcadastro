2a6e5fd85d0893a2c629ba67b07d877853e5538f
Crhistian <crhisnoriega@gmail.com>
2015-02-13 14:17:32 -0200

    geracao da planilha

--- src/main/java/br/com/tti/extratorcadastro/excel/ExcelDocumentoGenerador.java
+++ src/main/java/br/com/tti/extratorcadastro/excel/ExcelDocumentoGenerador.java
@@ -9,7 +9,6 @@ import java.text.SimpleDateFormat;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Vector;
-import java.util.logging.Level;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFDataFormat;
@@ -24,11 +23,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
-import br.com.tti.extratorcadastro.XMLInfo;
-
 public class ExcelDocumentoGenerador {
-
-	protected Vector<Object> infos;
 
 	protected Hashtable<String, String> headerdata;
 
@@ -36,150 +31,128 @@ public class ExcelDocumentoGenerador {
 
 	private SimpleDateFormat sdf;
 
-	private boolean islogs;
+	private HSSFWorkbook workbook;
+	private FileOutputStream out;
+	private HSSFSheet sheet;
+	private int counter;
 
-	public ExcelDocumentoGenerador(Hashtable<String, String> headerdata,
-			Vector<Object> infos) {
+	private String tmpFile;
+
+	public ExcelDocumentoGenerador(String tmpFile,
+			Hashtable<String, String> headerdata) {
 		super();
-		this.infos = infos;
+		this.tmpFile = tmpFile;
 		this.headerdata = headerdata;
-
 		this.sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	}
 
-	private void fillNFeAbbottRow(int rownum, Row row, XMLInfo info,
+	public void fillEmitenteNFe(
+			br.com.tti.extratorcadastro.xml.esquemas.nfe.TNFe.InfNFe.Emit emitente,
 			CellStyle cs) {
+		Row row = sheet.createRow(this.counter++);
+		row.createCell(0).setCellValue(
+				emitente.getCNPJ() != null ? emitente.getCNPJ() : emitente
+						.getCPF());
 
-		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
-
-		Cell cd = row.createCell(0);
-		cd.setCellValue(sdf.format(info.getDT_EMISSAO()));
-
-		Cell cd1 = row.createCell(1);
-		cd1.setCellValue(sdf.format(info.getDT_ENTRADA()));
-
-		row.createCell(2).setCellValue(info.getDOCNUM());
-		row.createCell(3).setCellValue(info.getNUM_NF());
-		row.createCell(4).setCellValue(info.getSERIE());
-
-		row.createCell(5).setCellValue(info.getCGC_CPF());
-		row.createCell(6).setCellValue(info.getTOTAL_DA_NOTA());
-
-		Cell celldest = row.createCell(7);
-		celldest.setCellValue(info.getChave());
-
-		Cell cellnum = row.createCell(8);
-		cellnum.setCellValue(info.getCnpjdest());
 	}
 
-	public void create() {
-		try {
-			fileout = new File(System.getProperty("user.home")
-					+ "/workbook.xls");
-			FileOutputStream out = new FileOutputStream(fileout);
+	public void createHeader() {
 
-			Workbook wb = new HSSFWorkbook();
-			// create a new sheet
-			Sheet s = wb.createSheet();
+	}
 
-			Row r = null;
-			Cell c = null;
-			// create 3 cell styles
-			CellStyle cs = wb.createCellStyle();
-			CellStyle cs2 = wb.createCellStyle();
-			CellStyle cs3 = wb.createCellStyle();
-			DataFormat df = wb.createDataFormat();
+	public File getFile() throws Exception {
 
-			// create 2 fonts objects
-			Font f = wb.createFont();
-			Font f2 = wb.createFont();
+		/*
+		 * // String strFormula = "SUM(B1:B" + (infos.size() + 1) + ")";
+		 * 
+		 * Cell cellserienum = row.createCell(0);
+		 * cellserienum.setCellValue("Somatorio do Valor das Notas");
+		 * 
+		 * Cell cell1 = row.createCell(1);
+		 * cell1.setCellType(HSSFCell.CELL_TYPE_FORMULA);
+		 * cell1.setCellFormula(strFormula);
+		 */
 
-			// set font 1 to 12 point type
-			f.setFontHeightInPoints((short) 12);
-			// make it blue
-			f.setColor((short) 0xc);
-			// make it bold
-			// arial is the default font
-			f.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		this.workbook.write(this.out);
+		this.out.close();
 
-			// set font 2 to 10 point type
-			f2.setFontHeightInPoints((short) 10);
-			// make it red
-			f2.setColor((short) Font.COLOR_RED);
-			// make it bold
-			f2.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		return this.fileout;
+	}
 
-			f2.setStrikeout(true);
+	public void init() throws Exception {
 
-			// set cell stlye
-			cs.setFont(f);
-			// set the cell format
-			cs.setDataFormat(df.getFormat("#,##0.0"));
+		this.fileout = new File(this.tmpFile);
+		this.out = new FileOutputStream(fileout);
+		this.workbook = new HSSFWorkbook();
+		this.sheet = workbook.createSheet();
 
-			// set a thin border
-			// cs2.setBorderBottom(cs2.BORDER_THIN);
-			// / fill w fg fill color
-			// cs2.setFillPattern((short) CellStyle.SOLID_FOREGROUND);
-			// set the cell format to text see DataFormat for a full list
-			cs2.setDataFormat(HSSFDataFormat.getBuiltinFormat("MM/dd/yyyy"));
+		Row r = null;
+		Cell c = null;
+		// create 3 cell styles
+		CellStyle cs = workbook.createCellStyle();
+		CellStyle cs2 = workbook.createCellStyle();
+		CellStyle cs3 = workbook.createCellStyle();
+		DataFormat df = workbook.createDataFormat();
 
-			// set the font
-			// cs2.setFont(f2);
+		// create 2 fonts objects
+		Font f = workbook.createFont();
+		Font f2 = workbook.createFont();
 
-			// set the sheet name in Unicode
-			wb.setSheetName(0, "testes");
+		// set font 1 to 12 point type
+		f.setFontHeightInPoints((short) 12);
+		// make it blue
+		f.setColor((short) 0xc);
+		// make it bold
+		// arial is the default font
+		f.setBoldweight(Font.BOLDWEIGHT_BOLD);
 
-			s.setColumnWidth(0, 400 * 10);
-			s.setColumnWidth(1, 400 * 10);
-			s.setColumnWidth(2, 400 * 14);
-			s.setColumnWidth(3, 400 * 14);
-			s.setColumnWidth(4, 400 * 30);
-			s.setColumnWidth(5, 400 * 14);
-			s.setColumnWidth(6, 400 * 14);
-			s.setColumnWidth(7, 400 * 34);
+		// set font 2 to 10 point type
+		f2.setFontHeightInPoints((short) 10);
+		// make it red
+		f2.setColor((short) Font.COLOR_RED);
+		// make it bold
+		f2.setBoldweight(Font.BOLDWEIGHT_BOLD);
 
-			boolean isheader = true;
+		f2.setStrikeout(true);
 
-			for (int rownum = 0; rownum < this.infos.size(); rownum++) {
-				Row row = s.createRow((short) (rownum + 1));
-				Object info = this.infos.get(rownum);
-				if (info instanceof XMLInfo) {
-					if (isheader) {
-						Row header = s.createRow(0);
-						header.createCell(0).setCellValue("\"dt_emissao\"");
-						header.createCell(1).setCellValue("\"dt_entrada\"");
-						header.createCell(2).setCellValue("\"docnum\"");
-						header.createCell(3).setCellValue("\"num_nf\"");
-						header.createCell(4).setCellValue("\"serie\"");
-						header.createCell(5).setCellValue("\"cgc_cpf\"");
-						header.createCell(6).setCellValue("\"tota_da_nota\"");
-						header.createCell(7).setCellValue("\"chave\"");
-						header.createCell(8).setCellValue("\"destinatario\"");
+		// set cell stlye
+		cs.setFont(f);
+		// set the cell format
+		cs.setDataFormat(df.getFormat("#,##0.0"));
 
-						isheader = false;
-					}
+		// set a thin border
+		// cs2.setBorderBottom(cs2.BORDER_THIN);
+		// / fill w fg fill color
+		// cs2.setFillPattern((short) CellStyle.SOLID_FOREGROUND);
+		// set the cell format to text see DataFormat for a full list
+		cs2.setDataFormat(HSSFDataFormat.getBuiltinFormat("MM/dd/yyyy"));
 
-					XMLInfo nfinfo = (XMLInfo) info;
-					this.fillNFeAbbottRow(rownum, row, nfinfo, cs2);
-				}
-			}
+		// set the font
+		// cs2.setFont(f2);
 
-			Row row = s.createRow(infos.size() + 1);
-			String strFormula = "SUM(B1:B" + (infos.size() + 1) + ")";
+		// set the sheet name in Unicode
+		this.workbook.setSheetName(0, "emitentes");
 
-			Cell cellserienum = row.createCell(0);
-			cellserienum.setCellValue("Somatorio do Valor das Notas");
+		this.sheet.setColumnWidth(0, 400 * 10);
+		this.sheet.setColumnWidth(1, 400 * 10);
+		this.sheet.setColumnWidth(2, 400 * 14);
+		this.sheet.setColumnWidth(3, 400 * 14);
+		this.sheet.setColumnWidth(4, 400 * 30);
+		this.sheet.setColumnWidth(5, 400 * 14);
+		this.sheet.setColumnWidth(6, 400 * 14);
+		this.sheet.setColumnWidth(7, 400 * 34);
 
-			Cell cell1 = row.createCell(1);
-			cell1.setCellType(HSSFCell.CELL_TYPE_FORMULA);
-			cell1.setCellFormula(strFormula);
+		Row header = this.sheet.createRow(this.counter++);
+		header.createCell(0).setCellValue("\"cnpj\"");
+		header.createCell(1).setCellValue("\"\"");
+		header.createCell(2).setCellValue("\"docnum\"");
+		header.createCell(3).setCellValue("\"num_nf\"");
+		header.createCell(4).setCellValue("\"serie\"");
+		header.createCell(5).setCellValue("\"cgc_cpf\"");
+		header.createCell(6).setCellValue("\"tota_da_nota\"");
+		header.createCell(7).setCellValue("\"chave\"");
+		header.createCell(8).setCellValue("\"destinatario\"");
 
-			wb.write(out);
-			out.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 	public File generatePDF() {
