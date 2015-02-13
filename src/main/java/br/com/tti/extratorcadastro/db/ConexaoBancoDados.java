e00855014d7d8fcafd71a76844b05095ae9f6520
Crhistian <crhisnoriega@gmail.com>
2015-02-13 11:08:46 -0200

    auto-geracao do jar com MANIFEST

--- src/main/java/br/com/tti/extratorcadastro/db/ConexaoBancoDados.java
+++ src/main/java/br/com/tti/extratorcadastro/db/ConexaoBancoDados.java
@@ -1,42 +1,47 @@
 package br.com.tti.extratorcadastro.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class ConexaoBancoDados {
 
 	private String url;
 	private String driver;
 	private String usuario;
 	private String senha;
+	private Connection conx;
 
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
+	public ConexaoBancoDados(String url, String driver, String usuario,
+			String senha) throws Exception {
+		super();
 		this.url = url;
-	}
-
-	public String getDriver() {
-		return driver;
-	}
-
-	public void setDriver(String driver) {
 		this.driver = driver;
-	}
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(String usuario) {
 		this.usuario = usuario;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
 		this.senha = senha;
+
+		Class.forName(this.driver);
+		this.conx = DriverManager.getConnection(this.url, this.usuario,
+				this.senha);
+	}
+
+	public List<String> findXMLs(Date fromData) throws Exception {
+		List<String> xmls = new ArrayList<String>();
+
+		String sql = "SELECT * FROM XMLINFO";
+		Statement stat = this.conx.createStatement();
+		ResultSet result = stat.executeQuery(sql);
+
+		while (result.next()) {
+			String xmlstring = result.getString("XMLSTRING");
+			xmls.add(xmlstring);
+		}
+
+		return xmls;
 	}
 
 }
